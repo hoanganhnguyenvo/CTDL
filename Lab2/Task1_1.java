330c92ccf8f5b49050ca325517e3e1d68df6a02a
hoanganhnguyenvo <baodgery.15@gmail.com>
2023-10-07 22:59:15 +0700

    Lab

--- Lab2/Task1_1.java
+++ Lab2/Task1_1.java
@@ -1,77 +1,61 @@
 package Lab2;
 
 public class Task1_1 {
+	
 	// S(n)=1-2+3-4+…+ ((-1)^(n+1) ).n , n>0
-	public static int getSn1(int n) {
-		// TODO
-		if (n == 1) {
-			return 1;
-		} else {
-			int sign = (n % 2 == 0) ? -1 : 1;
-			return sign * n + getSn1(n - 1);
+		public static int getSn1(int n) {
+			if (n == 1) {
+				return 1;
+			}else {
+				return (int) (getSn1(n - 1) + Math.pow(-1, n + 1) * n);
 		}
-	}
-
-	// S(n)=1+1.2+1.2.3+…+1.2.3…n, n>0
-	public static int getSn2(int n) {
-
-		if (n == 1) {
-			return 1;
-		} else {
-			return n * getSn2(n - 1);
 		}
-	}
+		// S(n)=1+1.2+1.2.3+…+1.2.3…n, n>0
 
-	public static int sum(int n) {
-		if (n == 1) {
-			return 1;
-		} else {
-			return getSn2(n) + sum(n - 1);
+		public static int getSn2(int n) {
+			if (n == 1) {
+				return 1;
+			} else {
+				return getSn2(n - 1) + factorial2(n);
+			}
 		}
-	}
-
-	// S(n)=1^2+2^2+3^2+....+n^2 , n>0
-	public static int getSn3(int n) {
-		if (n == 1) {
-			return 1;
-		} else {
-			return n * n + sum(n - 1);
+		// S(n)=1^2+2^2+3^2+....+n^2 , n>0
+		public static int getSn3(int n) {
+			if (n == 1) {
+				return 1;
+			}else {
+				return (int) (getSn3(n - 1) + Math.pow(n, 2));
+		}
+		}
+		// S(n)=1+1/2+1/(2.4)+1/(2.4.6)+…+1/(2.4.6.2n), n>=0
+		public static double getSn4(int n, int i) {
+			if (n == 0) {
+				return 1;
+			} else {
+				return 1.0 / factorial4(i) + getSn4(n - 1, i + 2);
+			}
 		}
 
-	}
-
-	// S(n)=1+1/2+1/(2.4)+1/(2.4.6)+…+1/(2.4.6.2n), n>=0
-	public static double getSn4(int n) {
-		if (n == 1 || n == 0) {
-			return 1;
-		} else {
-			return n * getSn4(n - 2);
+		public static int factorial2(int num) {
+			if (num == 0) 
+				return 1;
+			 else 
+				return num * factorial2(num - 1);
+			
 		}
 
-	}
-
-	public static double sum1(int n) {
-		if (n == 0) {
-			return 1;
-		} else {
-			double denominator = getSn4(2 * n);
-			return 1 / denominator + sum1(n - 1);
+		public static int factorial4(int num) {
+			if (num <= 1) {
+				return 1;
+			} else {
+				return num * factorial4(num - 2);
+			}
 		}
-	}
 
-	public static void main(String[] args) {
-		//1. S(n)=1-2+3-4+…+ ((-1) (n+1) ).n , n>0
-
-		System.out.println(getSn1(5));
-		//2. S(n)=1+1.2+1.2.3+…+1.2.3…n, n>0
-		
-		System.out.println(getSn2(5));
-		//3. S(n)=12+22+32+....+n2. n > 0
-		
-		
-		System.out.println(getSn3(5));
-		//4. S(n)=1+1/2+1/(2.4)+1/(2.4.6)+…+1/(2.4.6...2n), n>=0
-		
-		System.out.println(getSn4(5));
-	}
-}
+		public static void main(String[] args) {
+			int n = 3;
+			System.out.println(getSn1(n));
+			System.out.println(getSn2(n));
+			System.out.println(getSn3(n));
+			System.out.println(getSn4(n, 2));
+		}}
